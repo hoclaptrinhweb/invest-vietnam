20b7f79041aaccc2b5c29a262b0eb52b7db740d9
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@e6e66121-6a73-9bf8-afac-3b29f55a6106>
2014-03-27 15:56:19 +0000

    Create Project Invest

---
+++ Code/Invest/DataLayer/InvestContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using DomainClasses;
+
+namespace DataLayer
+{
+    public class InvestContext : DbContext
+    {
+        public InvestContext() : base("Invest") { }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Language> Language { get; set; }
+        public DbSet<LocalizedProperty> LocalizedProperty { get; set; }
+        public DbSet<LocaleStringResource> LocaleStringResource { get; set; }
+    }
+}
