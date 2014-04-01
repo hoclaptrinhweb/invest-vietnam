54de3b7b80bfec2cfb5e92ee263505dfcea08936
vonhatnam100689@gmail.com <vonhatnam100689@gmail.com@e6e66121-6a73-9bf8-afac-3b29f55a6106>
2014-04-01 08:28:14 +0000

    Cho phép sử dụng Async trong Console và MVC

--- trunk/Code/Invest/Console/Program.cs
+++ trunk/Code/Invest/Console/Program.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using DataLayer;
 using DataLayer.Migrations;
 using DomainClasses;
 
-namespace Console
+namespace ConsoleTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            CreateDatabase();
+            var task =Task.Factory.StartNew<int>(SlowOperation);
+            for(var i=0;i<100;i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Slow operation result: {0}", task.Result);
+            Console.WriteLine("Main complete on {0}", Thread.CurrentThread.ManagedThreadId);
 
+           // CreateDatabase();
+        }
+
+        static int SlowOperation()
+        {
+            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Slow operation started on {0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(2000);
+            Console.WriteLine("Slow operation complete on {0}", Thread.CurrentThread.ManagedThreadId);
+            return 42;
         }
 
         public static void CreateDatabase()
